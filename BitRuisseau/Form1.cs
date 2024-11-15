8eb4244c057981ec8f93891bea74e9e8eccac0db
ethanschafstall <ethans9999@gmail.com>
2024-11-15 15:05:53 +0100

    feat(select network interface UI): Adds UI components
    30m-WIP-#10 Adds network interface UI components init methods to Form1 class.

--- BitRuisseau/Form1.cs
+++ BitRuisseau/Form1.cs
@@ -1,10 +1,37 @@
+using BitRuisseau.services;
+using Microsoft.VisualBasic;
+using System.Net.NetworkInformation;
+using System.Reflection;
+
 namespace BitRuisseau
 {
     public partial class Form1 : Form
     {
+        List<NetworkInterface> networkInterfaces = NetworkInterfaceService.GetInterfaces();
+
+        const int TEXT_WIDTH = 6;
+        const int TEXT_HIGHT = 20;
         public Form1()
         {
             InitializeComponent();
+            InitInterfacesChecked();
+        }
+        private void InitInterfacesChecked()
+        {
+
+            // CheckedListBox for each network interface
+            CheckedListBox networkInterfaceList = new CheckedListBox
+            {
+                Location = new Point(160, 30),
+                Size = new Size(networkInterfaces.Select(netInt => netInt.Description.Count()).ToList().Max() * TEXT_WIDTH, networkInterfaces.Count * TEXT_HIGHT)
+            };
+
+            // Create checklist button for each available interface
+            foreach (var item in networkInterfaces)
+            {
+                networkInterfaceList.Items.Add(item.Description, false);
+            }
+            this.Controls.Add(networkInterfaceList);
         }
     }
 }
