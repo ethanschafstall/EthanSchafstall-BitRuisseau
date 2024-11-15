88d17899ec6deff949991da459dea79f0fe397da
ethanschafstall <ethans9999@gmail.com>
2024-11-15 14:46:37 +0100

    feat(select network interface): Adds logic
    13m-WIP- #9 Adds network interface service class, and method which returns all network interfaces

---
+++ BitRuisseau/services/NetworkInterface.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.NetworkInformation;
+using System.Diagnostics;
+
+namespace BitRuisseau.services
+{
+    internal static class NetworkInterfaceService
+    {
+        public static List<NetworkInterface> GetInterfaces() 
+        {
+            return NetworkInterface.GetAllNetworkInterfaces().ToList();
+        }
+    }
+}
