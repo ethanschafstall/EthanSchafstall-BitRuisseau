2653b2ebe9dc25366118710d300b11f15c2c4b7f
ethanschafstall <ethans9999@gmail.com>
2024-11-22 14:14:34 +0100

    feat(mqtt): creates mqtt service and config classes
    70m-WIP-#3 class for establishing the connection to the broker.

---
+++ BitRuisseau/configs/Mqtt_Config.cs
@@ -0,0 +1,17 @@
+﻿namespace BitRuisseau.configs
+{
+    public static class Mqtt_Config
+    {
+        public static string Broker = "inf-n510-p301";
+
+        public static int Port = 1883;
+
+        public static string ClientId = Guid.NewGuid().ToString();
+
+        public static string Topic = "test";
+
+        public static string Username = "ict";
+
+        public static string Password = "123";
+    }
+}
---
+++ BitRuisseau/services/Mqtt.cs
@@ -0,0 +1,44 @@
+﻿using MQTTnet;
+using MQTTnet.Client;
+using MQTTnet.Formatter;
+using MQTTnet.Server;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Windows.Forms.Design.AxImporter;
+
+namespace BitRuisseau.services
+{
+    public class Mqtt
+    {
+        private static IMqttClient _client;
+        private MqttClientOptions _options = new MqttClientOptions();
+        public Mqtt(string broker, int port, string clientId, string username, string password)
+        {
+            // Create a MQTT client factory
+            var factory = new MqttFactory();
+
+            // Create a MQTT client instance
+            _client = factory.CreateMqttClient();
+
+            // Create MQTT client options
+            _options = new MqttClientOptionsBuilder()
+                .WithTcpServer(broker, port)
+                .WithCredentials(username, password)
+                .WithClientId(clientId)
+                .Build();
+        }
+        public async Task ConnectToBroker()
+        {
+            var connectResult = await _client.ConnectAsync(_options);
+            _client.ConnectedAsync += e =>
+            {
+                Console.WriteLine($"Connected to Broker");
+                return Task.CompletedTask;
+            };
+        }
+    }
+}
