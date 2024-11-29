4baecdd86115ac4ec7b8eaa59ac574124dee7a00
ethanschafstall <ethans9999@gmail.com>
2024-11-29 15:36:27 +0100

    feat(FileManager tool): creates class for finding files in a given dir
    29m-DONE-#11

---
+++ BitRuisseau/tools/FileManager.cs
@@ -0,0 +1,37 @@
+﻿using BitRuisseau.Models;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitRuisseau.tools
+{
+    public static class FileManager
+    {
+        public static List<FileModel> GetFileInfo(string path)
+        {
+            List<FileModel> files = new List<FileModel>();
+
+            if (Directory.Exists(path))
+            {
+                string[] fileEntries = Directory.GetFiles(path);
+                foreach (string file in fileEntries)
+                {
+                    FileInfo fileInfo = new FileInfo(file);
+                    files.Add(new FileModel(fileInfo.Name, fileInfo.Length.ToString(), fileInfo.FullName, fileInfo.Extension));
+                }
+
+                //Recurse into subdirectories of this directory.
+                string[] subdirectoryEntries = Directory.GetDirectories(path);
+                foreach (string subdirectory in subdirectoryEntries)
+                {
+                    files.AddRange(GetFileInfo(subdirectory));
+                }
+            }
+            return files;
+        }
+    }
+}
