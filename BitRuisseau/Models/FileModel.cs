cfd6d7949ec4159912fbb12198fcb01597547cc0
ethanschafstall <ethans9999@gmail.com>
2024-11-29 15:38:45 +0100

    feat(fileModel): adds class for creating a file odjects
    15m-DONE-#12

---
+++ BitRuisseau/Models/FileModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitRuisseau.Models
+{
+    public class FileModel
+    {
+        public string Name { get; set; }
+        public string SizeInKb { get; set; }
+        public string Path { get; set; }
+        public string Extension { get; set; }
+        public FileModel(string name, string size, string path, string extension)
+        {
+            Name = name;
+            SizeInKb = size;
+            Path = path;
+            Extension = extension;
+        }
+
+    }
+}
