f27d7d884207c266b2e28e332810a99ec866ff02
ethanschafstall <ethans9999@gmail.com>
2024-11-29 15:37:10 +0100

    feat(catalog): creates class for user catalog
    10m-DONE-#12

---
+++ BitRuisseau/Catalog.cs
@@ -0,0 +1,15 @@
+﻿using BitRuisseau.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitRuisseau
+{
+    public class Catalog
+    {
+        public List<FileModel> fileModels { get; set; } = new List<FileModel>();
+        public Catalog(){ }
+    }
+}
